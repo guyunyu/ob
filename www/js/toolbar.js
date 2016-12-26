94843be3720490b23a608c189b811a3e203706d1
guyunyu <guyunyu@yahoo.com>
2016-12-26 11:24:06 +0800

    !

--- www/js/toolbar.js
+++ www/js/toolbar.js
@@ -19,6 +19,98 @@ ob.toolbar = {
 			});
 		});
 		if($$('.ob-search input.search-on-list').length === 1) {
+			var ajax_queue = [];
+			var ajax_delay = function( ) {
+				var autocomplete, query, render;
+				if(ajax_queue.length > 0) {
+					var index = ajax_queue.length - 1;
+					var e = ajax_queue[index];
+					autocomplete = e.a;
+					query = e.q;
+					render = e.r;
+					ajax_queue.splice(0, index + 1);
+				} else {
+					return;
+				}
+				autocomplete.showPreloader();
+				ob.ajax({
+					url: ob.url('/a/catalog/Item.List'),
+					method: 'GET',
+					data: {
+						q: query,
+						pageSize: 1,
+						pageOffset: 99999
+					},
+					success: function(dt) {
+						console.log(dt);
+						autocomplete.hidePreloader();
+						var results = [];
+						var json = JSON.parse(dt);
+						if(typeof json.count == 'number' && json.count > 0) {
+							var t = Template7.compile('<div class="item-title gray">{{left}}</div><div class="item-after dark">{{right}}</div>');
+							results.push({
+								id: 0,
+								q: query,
+								name: t({
+									left: 'show all items matching <b>' + ob.escapeHtml(query) + '</b>' + ' (' + json.count + ( parseInt(json.count, 10) > 1 ? ' items' : ' item' ) + ')',
+									right: ''
+								})
+							});
+							if(json.variables.facets) {
+								if(json.variables.facets.category_id) {
+									if(json.variables.facets.category_id.length > 0) {
+										results.push({
+											id: -1,
+											q: query,
+											name: t({
+												left: '',
+												right: 'items within selected category'
+											})
+										});
+									}
+									for(var index = 0; index < json.variables.facets.category_id.length && index < 5; index++) {
+										var item = json.variables.facets.category_id[index];
+										results.push({
+											id: results.length,
+											c: item.id,
+											q: query,
+											name: t({
+												left: query + ' (' + item.count + ( parseInt(item.count, 10) > 1 ? ' items' : ' item' ) + ')',
+												right: item.name
+											})
+										});
+									}
+								}
+								if(json.variables.facets.brand_id) {
+									if(json.variables.facets.brand_id.length > 0) {
+										results.push({
+											id: -2,
+											q: query,
+											name: t({
+												left: '',
+												right: 'items within selected brand'
+											})
+										});
+									}
+									for(var index = 0; index < json.variables.facets.brand_id.length && index < 5; index++) {
+										var item = json.variables.facets.brand_id[index];
+										results.push({
+											id: results.length,
+											r: item.id,
+											q: query,
+											name: t({
+												left: query + ' (' + item.count + ( parseInt(item.count, 10) > 1 ? ' items' : ' item' ) + ')',
+												right: item.name
+											})
+										});
+									}
+								}
+							}
+						}
+						render(results);
+					}
+				});
+			};
 			var i = fw.autocomplete({
 				input: '.ob-search input.search-on-list',
 				openIn: 'page',
@@ -52,81 +144,16 @@ ob.toolbar = {
 						'</label>' +
 					'</li>',
 				source: function (autocomplete, query, render) {
-					var results = [];
 					if (query.length === 0) {
-						render(results);
+						render([]);
 						return;
 					}
-					autocomplete.showPreloader();
-					ob.ajax({
-						url: ob.url('/a/catalog/Item.List'),
-						method: 'GET',
-						data: {
-							q: query,
-							pageSize: 1,
-							pageOffset: 99999
-						},
-						success: function(dt) {
-							autocomplete.hidePreloader();
-							var json = JSON.parse(dt);
-							if(typeof json.count == 'number' && json.count > 0) {
-								var t = Template7.compile('<div class="item-title gray">{{left}}</div><div class="item-after dark">{{right}}</div>');
-								results.push({
-									id: 0,
-									q: query,
-									name: t({
-										left: 'show all items matching <b>' + ob.escapeHtml(query) + '</b>' + ' (' + json.count + ( parseInt(json.count, 10) > 1 ? ' items' : ' item' ) + ')',
-										right: ''
-									})
-								});
-								if(json.variables.facets.category_id.length > 0) {
-									results.push({
-										id: -1,
-										q: query,
-										name: t({
-											left: '',
-											right: 'items within selected category'
-										})
-									});
-								}
-								for(var index = 0; index < json.variables.facets.category_id.length && index < 5; index++) {
-									var item = json.variables.facets.category_id[index];
-									results.push({
-										id: results.length,
-										c: item.id,
-										q: query,
-										name: t({
-											left: query + ' (' + item.count + ( parseInt(item.count, 10) > 1 ? ' items' : ' item' ) + ')',
-											right: item.name
-										})
-									});
-								}
-								if(json.variables.facets.brand_id.length > 0) {
-									results.push({
-										id: -2,
-										q: query,
-										name: t({
-											left: '',
-											right: 'items within selected brand'
-										})
-									});
-								}
-								for(var index = 0; index < json.variables.facets.brand_id.length && index < 5; index++) {
-									var item = json.variables.facets.brand_id[index];
-									results.push({
-										id: results.length,
-										r: item.id,
-										q: query,
-										name: t({
-											left: query + ' (' + item.count + ( parseInt(item.count, 10) > 1 ? ' items' : ' item' ) + ')',
-											right: item.name
-										})
-									});
-								}
-							}
-							render(results);
-						}
+					ajax_queue.push({
+						a: autocomplete,
+						q: query,
+						r: render
 					});
+					setTimeout(ajax_delay, 500);
 				},
 				onOpen: function( autocomplete ) {
 				},
